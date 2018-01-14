7519c526bf50b9bc381cb1b0863a1d66779ed978
KatNms <knemes@bu.edu>
2018-01-14 14:48:50 -0800

    Add files via upload

---
+++ Lab5/Lab5/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(808, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(719, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1402, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 38);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(734, 459);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(340, 407);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 915);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(544, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter a starting integer (0-1,000,000,000):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1130, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(266, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter count (1-100):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1850, 1041);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Palindrones by Katrina Nemes";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab5/Lab5/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool click;
+        int count_input;
+        int starting_input;
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Font title_font = new Font("Arial MT", 24, FontStyle.Bold);
+            e.Graphics.DrawString("Find Numeric Palindrones", title_font, Brushes.Black, 130, 30);
+            title_font.Dispose();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int is_valid_start;
+            int is_valid_count;
+
+
+            listBox1.Items.Clear();     //clear previous items
+
+            if (Int32.TryParse(textBox1.Text, out is_valid_start) == true && Int32.TryParse(textBox2.Text, out is_valid_count) == true)
+            {
+
+                count_input = Convert.ToInt32(textBox2.Text);
+                starting_input = Convert.ToInt32(textBox1.Text);
+
+                click = true;
+                make_palindrone();
+                
+            }
+            else
+            {
+                label1.Text = "Please enter a positive integer within range.";
+            }
+            
+            
+
+        }
+
+
+        private void make_palindrone()
+        {
+
+            bool check = (starting_input >= 0) && (starting_input <= 1000000000) && (count_input >= 1) && (count_input <= 100);
+            
+
+            if (click)  //if the generate button was clicked
+            {
+                /*check if input values meet conditions:
+                 * 1. inputs can be parsed, therefore they are valid intergers
+                 * 2. input is within 0-1 billion or 1-100
+                 */
+                if (check == false)
+                {
+                    label1.Text = "Please enter a positive integer within range.";
+
+                }
+
+                else
+                {
+                    label1.Text = "";
+                    for (int ii = 0; ii < count_input; ii++)
+                    {
+                        //suggested class method
+                        string initial_input = starting_input.ToString();
+                        char[] palindrone_array = initial_input.ToCharArray();
+                        Array.Reverse(palindrone_array);
+                        string is_reversed = new string(palindrone_array);
+
+                        //make palindromes starting from input integer in textBox1 and add them to listBox1
+                        //test for a palindrome by converting the number to a string, reversing the string and comparing to the original
+                        if (initial_input == is_reversed)
+                        {
+                            //if it is a palindrome, add it to the list box
+                            listBox1.Items.Add(initial_input);
+                        }
+                        else
+                        {
+                            --ii;
+                        }
+
+                        ++starting_input;
+
+                    }
+
+
+                }
+
+            }
+        }
+
+
+        //need the following functions to exist but they dont do anything
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
