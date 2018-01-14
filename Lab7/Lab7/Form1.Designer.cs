ba1f9b9910c9ddcb90c36edb783e3163e5814ec7
KatNms <knemes@bu.edu>
2018-01-14 14:50:49 -0800

    Add files via upload

---
+++ Lab7/Lab7/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(656, 38);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 404);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(804, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 38);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 554);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(261, 72);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(448, 554);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(258, 72);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Decyrpt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "All files (*.*)|*.*|Encrypted files (*.enc)|*.enc";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1489, 822);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "File Encrypt/Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Lab7/Lab7/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        private string key;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //browse file button, opens file dialoge
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                /*set the string in textbox to whatever filename you choose 
+                in the open dialoge*/
+            }
+
+        }
+
+        private bool get_Key()
+        {
+            //obtain whatever key entered
+            if(textBox2.Text == "")
+            {
+                //check if nothing is entered in the key text box
+                MessageBox.Show("Enter a key please!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return false;
+            }
+
+            key = textBox2.Text; //set key to whatever was typed into the key textbox
+            return true;
+        }
+
+        //decrypt button
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            /*Decryption instructions
+             * This same algorithm is used to decrypt the file.
+             * Two consequitive XOR operations cancel each other
+             */
+
+            if (!get_Key())
+            {
+                return;
+            }
+            string text1 = textBox1.Text;
+
+            if (!text1.EndsWith(".enc"))
+            {
+                //check if file is not encoded
+                MessageBox.Show("This is not a .enc file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+
+            string text2 = text1.Substring(0, text1.Length - 3);
+            //permission to overwrite file
+            if (File.Exists(text2) && MessageBox.Show("Output file exists. Do you want to overwrite?", "File Exists", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            //load the cursor
+            Cursor = Cursors.WaitCursor;
+            Form1.encrypt_Data(text1, text2, key);
+            Cursor = Cursors.Default;
+        }
+
+        //encrypt button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //event for when you click the encyrpt button
+
+            if (!get_Key())
+            {
+                //if get_key wasnt called, aka you have no key
+                return;
+            }
+
+            string text1 = textBox1.Text;   //from filebox1 which is where you get the filename
+            string text2 = text1 + ".enc"; //add .enc filetype for "encoded"
+
+            if (File.Exists(text2) && MessageBox.Show("Output file exists. Do you want to overwrite?", "File Exists", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            //load the cursor
+            Cursor = Cursors.WaitCursor;
+            Form1.encrypt_Data(text1, text2, key);
+            Cursor = Cursors.Default;
+
+        }
+
+        //function to encrypt file
+        private static void encrypt_Data(string input_Name, string output_Name, string key)
+        {
+            //need filestreams, declare null 
+            FileStream stream1 = null;  //input
+            FileStream stream2 = null;  //output
+
+            try
+            {
+                //create a new filestream for hte input file/output file, add attributes like read and write
+                stream1 = new FileStream(input_Name, FileMode.Open, FileAccess.Read);
+                stream2 = new FileStream(output_Name, FileMode.OpenOrCreate, FileAccess.Write);
+                stream2.SetLength(0L);
+            }
+            catch
+            {
+                MessageBox.Show("Error :-(( Could not open source or destination file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+                //if input stream or output stream is null
+                if(stream1 != null)
+                {
+                    //close
+                    stream1.Close();
+                }
+                if(stream2 != null)
+                {
+                    //close
+                    stream2.Close();
+                }
+
+                return;
+          
+            }
+
+            /*Instructions for encryption algorithm
+             * The encryption is done by performing an exclusive OR (^ operator) on each byte of the file and a corresponding character in the key cast to a byte. 
+             * Byte 1 of the file is XORed with byte 1 of the key. 
+             * Byte 2 of the file is XORed with byte 2 of the key etc. 
+             * After using the last byte of the key we go back to the first byte of the key wrapping around as many times as needed. 
+             * The character in the Unicode key string is cast to a byte (byte). 
+             */
+
+
+            int num = 0;
+            int length = key.Length;    //get key length
+            int num2;
+
+            while((num2 = stream1.ReadByte()) != -1)
+            {
+                byte b = (byte)key[num];    //byte1
+                byte val = (byte)(num2 ^ (int)b);   //byte2
+
+                stream2.WriteByte(val); //write byte 2 to output file
+                num++;  //increment num
+
+                if (num == length)  //if num == length of key then reset to 0
+                {
+                    num = 0;
+                }
+
+
+            }
+
+            //close the streams
+            stream2.Close();
+            stream1.Close();
+
+            MessageBox.Show("Congrats! The operation completed successfully.");
+        }
+
+    }
+}
