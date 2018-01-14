b5a3c895f27b43f9ea4d30ea758e73425392c59c
KatNms <knemes@bu.edu>
2018-01-14 14:35:24 -0800

    Add files via upload

---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics name = e.Graphics;
+
+            name.DrawString("Katrina Nemes", Font, Brushes.Black, 100, 100);
+        }
+    }
+}
